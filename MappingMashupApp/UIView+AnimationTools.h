9019870bc7ee4bddc6e006ab23f1b25cba7c5908
bitey <bitey@outlook.com>
2013-03-24 20:53:23 -0500

    adding a category folder for animations

---
+++ MappingMashupApp/UIView+AnimationTools.h
@@ -0,0 +1,13 @@
+//
+//  UIView+AnimationTools.h
+//  MappingMashupApp
+//
+//  Created by StopBitingMe on 3/24/13.
+//  Copyright (c) 2013 The Peanut Gallery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIView (AnimationTools)
+
+@end
---
+++ MappingMashupApp/UIView+AnimationTools.m
@@ -0,0 +1,13 @@
+//
+//  UIView+AnimationTools.m
+//  MappingMashupApp
+//
+//  Created by StopBitingMe on 3/24/13.
+//  Copyright (c) 2013 The Peanut Gallery. All rights reserved.
+//
+
+#import "UIView+AnimationTools.h"
+
+@implementation UIView (AnimationTools)
+
+@end
