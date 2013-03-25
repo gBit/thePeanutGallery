a1e6a84c06ee19d36dbbb6b4a0f1bd9b44b60173
gBit <gBit@bitMail.com>
2013-03-25 10:31:38 -0500

    updates to location services

---
+++ MappingMashupApp/PGLocationManager.h
@@ -0,0 +1,13 @@
+//
+//  PGLocationManager.h
+//  MappingMashupApp
+//
+//  Created by gBit on 3/25/13.
+//  Copyright (c) 2013 The Peanut Gallery. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PGLocationManager : NSObject
+
+@end
---
+++ MappingMashupApp/PGLocationManager.m
@@ -0,0 +1,13 @@
+//
+//  PGLocationManager.m
+//  MappingMashupApp
+//
+//  Created by gBit on 3/25/13.
+//  Copyright (c) 2013 The Peanut Gallery. All rights reserved.
+//
+
+#import "PGLocationManager.h"
+
+@implementation PGLocationManager
+
+@end
